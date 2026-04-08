34e56f690da84e94870a7de6018409ea438ede8c
aayush-9310 <aayushnegi9310@gmail.com>
2026-04-08 21:24:56 +0530

    Initialize Express server with TypeScript and MongoDB connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,32 @@
 
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+async function main(){
+    try{
+        await mongoose.connect(process.env.MONGO_URL!);
+        app.listen(3000, ()=> console.log("running on port 3000"))
+
+        console.log("db connected")
+    }
+    catch(e){
+        console.log("db error : " ,e)
+    }
+}
+
+app.get("/", (req,res)=>{
+    res.json({
+        msg : "Waste detection "
+    })
+})
+
+main();
+
